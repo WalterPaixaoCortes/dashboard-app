02cf5e63a454b3ea69a7bc93f41ff77ab318f314
Walter Ritzel Paixao Cortes <walter.paixao-cortes@acad.pucrs.br>
2017-03-08 20:11:09 -0300

    making web_app externally available

--- web_app.py
+++ web_app.py
@@ -102,6 +102,6 @@ def format_datetime(value):
     return datetime.datetime.strptime(value,'%Y-%m-%d %H:%M:%S.%f').strftime('%d/%m/%Y %H:%M:%S')
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host="0.0.0.0", port="8080")
 
 
